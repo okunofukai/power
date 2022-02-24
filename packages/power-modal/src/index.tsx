497a73fb5dfaefe56e19edd1996d4959e22dfc5a
Dave Cordero <daverencordero@gmail.com>
2022-02-24 20:17:15 +0800

    chore: added facepaint dependency

--- packages/power-modal/src/index.tsx
+++ packages/power-modal/src/index.tsx
@@ -1,22 +1,39 @@
+import { CSSObject } from "@emotion/react";
 import React, { FC, useCallback } from "react";
 import ReactDOM from "react-dom";
-import { css } from "@emotion/react";
 
-export interface PowerModalProps {}
+export interface PowerModalProps {
+	showOverlay?: boolean;
+}
 
 export const PowerModal: FC<PowerModalProps> = (props) => {
+	const { showOverlay } = props;
+
 	const BodyPortal: FC = useCallback(
 		(props) => ReactDOM.createPortal(props.children, document.body),
 		[]
 	);
 
-	const modalWrapperStyles = css`
-		background-color: red;
-	`;
+	const modalWrapperStyles = {
+		color: "yellow",
+	} as CSSObject;
+
+	const overlayStyles = {
+		position: "fixed",
+		height: "100vh",
+		width: "100vw",
+		top: "0",
+		left: "0",
+		zIndex: -1,
+		backgroundColor: "rgba(0,0,0,0.45)",
+	} as CSSObject;
 
 	return (
 		<BodyPortal>
-			<div css={modalWrapperStyles}>Test</div>
+			<div css={modalWrapperStyles}>
+				<div></div>
+				{showOverlay && <div css={overlayStyles} />}
+			</div>
 		</BodyPortal>
 	);
 };
