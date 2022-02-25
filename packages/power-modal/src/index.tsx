c1e242ea807ef12bd61db00a3318811b3006a685
Dave Cordero <daverencordero@gmail.com>
2022-02-25 16:46:24 +0800

    feat: implemented root imperative handler

--- packages/power-modal/src/index.tsx
+++ packages/power-modal/src/index.tsx
@@ -1,41 +1,103 @@
 import { CSSObject } from "@emotion/react";
-import React, { FC, useCallback } from "react";
+import React, {
+	Dispatch,
+	FC,
+	forwardRef,
+	SetStateAction,
+	useCallback,
+	useImperativeHandle,
+	useState,
+} from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-export interface PowerModalProps {
-	showOverlay?: boolean;
-}
-
-export const PowerModal: FC<PowerModalProps> = (props) => {
-	const { showOverlay } = props;
-
-	const BodyPortal: FC = useCallback(
-		(props) => ReactDOM.createPortal(props.children, document.body),
-		[]
-	);
-
-	const modalWrapperStyles = {
-		color: "yellow",
-	} as CSSObject;
-
-	const overlayStyles = {
+const usePowerModalStyles = () => {
+	const [modalWrapperStyles, setModalWrapperStyles] = useState<CSSObject>({
 		position: "fixed",
 		height: "100vh",
 		width: "100vw",
 		top: "0",
 		left: "0",
-		zIndex: -1,
-		backgroundColor: "rgba(0,0,0,0.45)",
-	} as CSSObject;
+	});
 
-	return (
-		<BodyPortal>
-			<div css={modalWrapperStyles}>
-				<div></div>
-				{showOverlay && <div css={overlayStyles} />}
-			</div>
-		</BodyPortal>
+	const [overlayStyles, setOverlayStyles] = useState<CSSObject>({
+		zIndex: -1,
+		position: "absolute",
+		top: "0",
+		left: "0",
+		height: "100%",
+		width: "100%",
+		backgroundColor: "rgba(0,0,0,0.45)",
+	});
+
+	const [modalContainerStyles, setModalContainerStyles] = useState<CSSObject>(
+		{
+			color: "yellow",
+		}
 	);
+
+	return {
+		overlayStyles,
+		setOverlayStyles,
+		modalWrapperStyles,
+		setModalWrapperStyles,
+		modalContainerStyles,
+		setModalContainerStyles,
+	};
+};
+
+export interface PowerModalProps {
+	showOverlay?: boolean;
+	prefixCls?: string;
+}
+
+export interface PowerModalRef {
+	setOverlayStyles: Dispatch<SetStateAction<CSSObject>>;
+	setModalWrapperStyles: Dispatch<SetStateAction<CSSObject>>;
+	setModalContainerStyles: Dispatch<SetStateAction<CSSObject>>;
+}
+
+export const PowerModal = forwardRef<PowerModalRef, PowerModalProps>(
+	(props, ref) => {
+		const { showOverlay, prefixCls } = props;
+
+		const {
+			overlayStyles,
+			setOverlayStyles,
+			modalWrapperStyles,
+			setModalWrapperStyles,
+			modalContainerStyles,
+			setModalContainerStyles,
+		} = usePowerModalStyles();
+
+		useImperativeHandle(ref, () => ({
+			overlayStyles,
+			setOverlayStyles,
+			modalWrapperStyles,
+			setModalWrapperStyles,
+			modalContainerStyles,
+			setModalContainerStyles,
+		}));
+
+		const BodyPortal: FC = useCallback(
+			(props) => ReactDOM.createPortal(props.children, document.body),
+			[]
+		);
+
+		return (
+			<BodyPortal>
+				<div css={modalWrapperStyles} className={prefixCls}>
+					<div css={modalContainerStyles}>TEST</div>
+					{showOverlay && <div css={overlayStyles} />}
+				</div>
+			</BodyPortal>
+		);
+	}
+);
+PowerModal.displayName = "PowerModal";
+PowerModal.propTypes = {
+	showOverlay: PropTypes.bool,
+	prefixCls: PropTypes.string,
 };
 
 export default PowerModal;
--- playgrounds/react-17/src/App.tsx
+++ playgrounds/react-17/src/App.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
-import { PowerModal } from "@okunofukai/power-modal/src";
+import { PowerModal, PowerModalRef } from "@okunofukai/power-modal/src";
 
 function App() {
+	const modalRef = useRef<PowerModalRef>({} as PowerModalRef);
+
+	useEffect(() => {
+		console.log(modalRef);
+	}, [modalRef]);
+
 	return (
 		<div className="App">
-			<PowerModal />
+			<PowerModal ref={modalRef} showOverlay={true} />
 		</div>
 	);
 }
