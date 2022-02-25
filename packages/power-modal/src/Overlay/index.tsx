117cb5fbffbf87f132ebe728e22abe7f7dc0f251
Dave Cordero <daverencordero@gmail.com>
2022-02-25 18:37:39 +0800

    feat: added overlay opacity options

--- packages/power-modal/src/Overlay/index.tsx
+++ packages/power-modal/src/Overlay/index.tsx
@@ -3,6 +3,7 @@ import React, { FC, useEffect } from "react";
 import PropTypes from "prop-types";
 
 export interface OverlayOptions {
+	opacity?: number | null;
 	pointerEvents?: boolean | null;
 }
 
@@ -23,8 +24,11 @@ export const Overlay: FC<OverlayProps> = (props) => {
 
 	useEffect(() => {
 		if (!overlayOptions) return;
-		const { pointerEvents } = overlayOptions;
-		setOverlayStyles({ pointerEvents: pointerEvents ? "auto" : "none" });
+		const { pointerEvents, opacity } = overlayOptions;
+		setOverlayStyles({
+			pointerEvents: pointerEvents ? "auto" : "none",
+			backgroundColor: `rgba(0,0,0,${opacity || 0.25})`,
+		});
 	}, [overlayOptions]);
 
 	return <div css={overlayStyles} />;
@@ -33,6 +37,7 @@ Overlay.propTypes = {
 	overlay: PropTypes.oneOfType([
 		PropTypes.bool,
 		PropTypes.shape({
+			opacity: PropTypes.number,
 			pointerEvents: PropTypes.bool,
 		}),
 	]),
